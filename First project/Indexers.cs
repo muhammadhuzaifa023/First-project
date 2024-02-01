ae57f309a59c84e3bfea1f7e16d87d163276ea84
TATHEER\TSQA-10 <muhammadhuzaifa023@gmail.com>
2024-02-01 15:17:32 +0500

    changed

---
+++ First project/Indexers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_project
+{
+    class MyCollection
+    {
+        private string[] data = new string[5];
+
+        // Indexer declaration
+        public string this[int index]
+        {
+            get
+            {
+                // Accessing elements using the index
+                if (index >= 0 && index < data.Length)
+                    return data[index];
+                else
+                    return "Index out of range";
+            }
+            set
+            {
+                // Setting elements using the index
+                if (index >= 0 && index < data.Length)
+                    data[index] = value;
+                else
+                    Console.WriteLine("Index out of range");
+            }
+        }
+    }
+
+
+
+
+    internal class Indexers
+    {
+        //Indexers provide a more generalized and flexible way to access elements within a class or structure compared to directly accessing elements in an array.
+        //While arrays allow direct access using square bracket notation, indexers enable you to define custom logic for accessing elements in your own types.
+
+        static void Main(string[] args)
+
+        {
+            MyCollection collection = new MyCollection();
+
+            // Using the indexer to set and get values
+            collection[0] = "Item 1";
+            collection[1] = "Item 2";
+            collection[2] = "Item 3";
+
+            Console.WriteLine(collection[0]); // Output: Item 1
+            Console.WriteLine(collection[1]); // Output: Item 2
+            Console.WriteLine(collection[3]); // Output: Index out of range
+            Console.WriteLine("This is Indexers Program ");
+        }
+    }
+}
---
+++ First project/Sealedclass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_project
+{
+    //  JB bhi hum sealed class bunnatay haii to woo kesi or class say inherit nh hoosaktii woo kesi or class ki Parent NH bunsaktii 
+
+    // why we can create Sealed Classes when we have critical information of Systeminformation that we cannot inherit with other class
+    // Koi bhi third party apki clas koo change nh kerpaya ya uski information koo too hum usko sealed class bunnadutay haii 
+    // When you declare a class as sealed, it means that the class cannot be inherited by other classes.
+    // Sealed  Methods
+
+
+    
+    sealed class Base
+    {
+        public string Name { get; set; }
+    }
+
+    //sealed class Derived : Base
+    //{
+    //    public int Age { get; set; }
+    //}
+
+
+
+
+
+    // In C#, the virtual keyword is used to indicate that a method or property in a base class is intended to be overridden by derived classes.
+    // Sealed Method hum sirf Method overriding ka saath use kertay haii 
+    // Sealed method is always an override method of sealed class.\
+    // Sealed keyword can not use with method hiding.
+
+
+
+    
+
+
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Generic animal sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    internal class Sealedclass
+    {
+        static void Main(string[] args)
+        {
+            //Derived derivedInstance = new Derived();
+            //derivedInstance.Name = "John";
+            //derivedInstance.Age = 30;
+
+            //Console.WriteLine($"Name: {derivedInstance.Name}, Age: {derivedInstance.Age}");
+
+            
+        }
+        
+    }
+}
